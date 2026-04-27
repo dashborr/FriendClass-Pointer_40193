7bd75fabd006c6b75fb12f21380e08f8406b908a
dashborr <danisarfas7@gmail.com>
2026-04-27 15:12:15 +0700

    menambahkan void setNama, void setNama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -9,3 +9,16 @@ private:
 public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+void setNama()
+{
+    mahasiswa joko;
+    setNama(joko, "Joko Kumat");
+    return 0;
+}
